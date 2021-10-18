ca95afe5294efef86f203a00d23fa4500144d2a2
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-17 17:35:51 -0700

    add initial questions

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+
+
+const questions = [
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is your id number?",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+    },
+]
