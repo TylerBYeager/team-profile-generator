dfa9c386225b4e7981723e1ead01fb2c7ece56a9
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-18 20:58:26 -0700

    add async and await for role questions

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 const fs = require('fs');
 const util = require('util');
 const inquirer = require('inquirer');
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
+const teamInfo = [];
 
-const questions = [
-    {
-        type: "input",
-        message: "What is your name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "What is your id number?",
-        name: "id",
-    },
-    {
-        type: "input",
-        message: "What is your email?",
-        name: "email",
-    },
-];
+const questions = async () => {
+    const answers = await inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the team member's name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is their employee ID number?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What is their email?",
+            name: "email",
+        },
+        {
+            type: "list",
+            message: "Please select what job this employee performs.",
+            choices: ["manager", "engineer", "intern"],
+            name: "role",
+        },
 
+])
+        if (answers.role === "manager") {
+            const manager = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your office number?",
+                    name: "number",
+                },
+            ])
+            const newManager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                manager.number,
+            );
+            teamInfo.push(newManager);
+            console.log(teamInfo);
+}
+};
+questions();
 
-inquirer
- .prompt(questions)
- .then((response) => {
-     console.log(response);
- });
+// inquirer
+//  .prompt(questions)
+//  .then((response) => {
+//      console.log(response);
+//  });
